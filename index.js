b9c4f77e0e4662074e3429563e211bfceb9bc5eb
casprine <casprine.001@gmail.com>
2019-05-09 23:18:30 +0000

    [ADD] added project based questions

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 import arg from 'arg';
+import { async } from 'rxjs/internal/scheduler/async';
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
-      '--git': Boolean,
-      '--yes': Boolean,
-      '--install': Boolean,
-      '-g': '--git',
-      '-y': '--yes',
-      '-i': '--install',
+      '-V': Boolean,
+      '--version': Boolean,
     },
-    {
-      argv: rawArgs.slice(2),
-    },
+    { argv: rawArgs.slice(2) },
   );
+
   return {
-    skipPrompts: args['--yes'] || false,
-    git: args['--git'] || false,
-    template: args._[0],
-    runInstall: args['--install'] || false,
+    version: args['-v'] || false,
+    function: args._[0], // get specific function to run
+    projectName: args._[1], // get project name and create folder with it
+    stack: args._[2],
   };
 }
 
-export function cli(args) {
+async function promptForMissingOptions() {
+  const defaultTemplate = 'default';
+  const defaultStack = 'react';
+
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate,
+      stack: options.stack || defaultStack,
+    };
+  }
+
+  const questions = [];
+
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'stack',
+      message: 'Please choose which stack to use',
+      choices: ['React', 'Nextjs', 'Gatsby'],
+      defaultStack: defaultStack,
+    });
+  }
+
+  // add more state mangement services and move to list [redux, unstate]
+  if (!options.template) {
+    question.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Do you want Mobx include ?',
+      default: false,
+    });
+  }
+}
+
+export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
-  console.log(options);
+  console.log(JSON.stringify(options, null, 2));
 }
