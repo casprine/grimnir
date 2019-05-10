b819694f5d036e50d51479ca2f9d93a7b8b84fbf
casprine <casprine.001@gmail.com>
2019-05-10 13:51:28 +0000

    [add] added more project-focused questions

--- index.js
+++ index.js
@@ -23,5 +23,4 @@ export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
   options = await createProductQuestions(options);
   await createNewProject(options);
-  console.log(JSON.stringify(options, null, 2));
 }
